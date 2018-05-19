3fe1877385213ad6df5e13679506cf6768b10917
Abel Martell <amartellram@gmail.com>
2018-05-19 16:23:01 -0500

    init commit

---
+++ visitas-backend-endpoint/src/main/java/com/amspace/visitas/endpoint/Application.java
@@ -0,0 +1,16 @@
+package com.amspace.visitas.endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(
+		scanBasePackages = { 
+				"com.amspace.visitas.endpoint.config",
+				})
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
